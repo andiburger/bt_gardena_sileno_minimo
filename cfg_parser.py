379140f11616c1d130fb21e7bda5b2471b4db94f
Andreas <andiburger@gmail.com>
2025-05-19 20:01:16 +0200

    add pin support in cfg file

--- cfg_parser.py
+++ cfg_parser.py
@@ -30,7 +30,7 @@ class GardenaCfg():
     def parse(self):
         self.config.read('cfg.ini')
         result = {"mower":{},"mqtt":{}}
-        result["mower"].update({"address":self.config["mower"]["address"]})
+        result["mower"].update({"address":self.config["mower"]["address"], "pin":self.config["mower"]["pin"]})
         result["mqtt"].update({"broker":self.config["mqtt"]["broker"], "port":self.config["mqtt"]["port"], "topic":self.config["mqtt"]["topic"], "topic_cmd":self.config["mqtt"]["topic_cmd"]})
         return result
 
